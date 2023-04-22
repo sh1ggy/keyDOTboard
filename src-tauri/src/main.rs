8440bcd68a046e6f7467a29ede133dc436931c1a
anhad <anhadrs@gmail.com>
2023-04-22 21:50:47 +1000

    rust working read rfid

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,22 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+mod serial;
+
+use std::{io, num::ParseIntError};
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    serial::read_rfid();
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
 }
